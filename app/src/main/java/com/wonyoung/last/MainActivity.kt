805e238cbdaa80c804abec2e41ad3af654633cae
wonyounglee <lwy970327@naver.com>
2022-07-13 19:29:30 +0900

    change view location

--- app/src/main/java/com/wonyoung/last/MainActivity.kt
+++ app/src/main/java/com/wonyoung/last/MainActivity.kt
@@ -2,10 +2,15 @@ package com.wonyoung.last
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner_address)
+        spinner.adapter = ArrayAdapter.createFromResource(this,R.array.itemList,android.R.layout.simple_spinner_item)
     }
 }
